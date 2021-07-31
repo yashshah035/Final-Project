bef79e785fc3654ad48176208c8138ac6327af12
yashshah035 <88094006+yashshah035@users.noreply.github.com>
2021-07-31 10:08:06 +0530

    Final Project

---
+++ Project Letsupgrade.py
@@ -0,0 +1,6 @@
+cel = float(input("Enter temperature in celsius: "))
+fah = (cel * 9/5) + 32
+
+print('*********************************')
+print('Temperature in Fahrenheit: ',fah)
+print('*********************************')
